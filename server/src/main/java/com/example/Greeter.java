bff1d98503820a279d48be9534e3e46f2ad768a4
Damion <damionhanson@msn.com>
2022-11-04 21:37:35 -0700

    Trigger pipeline with webhook

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
+    //Trigger Jenkins
     return String.format("Hello Avinash, %s!", someone);
   }
 }
